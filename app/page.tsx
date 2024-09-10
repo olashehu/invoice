a79a69a94b082c206eb1f41360c009900b0ba276
olashehu <abdulkadirshehu53.sa@gmail.com>
2024-09-09 23:47:56 -0700

    modifying home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,11 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-        {/* ... */}
+    <main className="flex min-h-screen flex-col">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-42">
+        {/* <AcmeLogo /> */}
       </div>
-      <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+      {/* <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
         <Image
           src="/hero-desktop.png"
           width={1000}
@@ -26,7 +25,7 @@ export default function Page() {
           className="block md:hidden"
           alt="Screenshot of the dashboard project showing mobile version"
         />
-      </div>
+      </div> */}
     </main>
   );
 }
